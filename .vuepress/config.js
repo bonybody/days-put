94d6a121e6d2d4e8564b72c17a0d00b43101798f
ubuntu-yumaa <yumaaa312@gmail.com>
2020-08-11 08:44:35 +0900

    add social share plugin

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,108 +1,114 @@
 module.exports = {
-  "title": "DAYS PUT",
-  "description": "日々得られる知識を整頓する技術系備忘録",
-  "dest": "public",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "link",
-      {
-        "rel": "stylesheet",
-        "href": "https://fonts.googleapis.com/css?family=Anton"
-      }
-    ],
-    [
-      "link",
-      {
-        "rel": "stylesheet",
-        "href": "https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "domain": 'https://daysput.netlify.app',
-    "vssueConfig": {
-      "platform": 'github',
-      "owner": 'bonybody',
-      "repo": 'daysput',
-      "clientId": '3c1c6993671e8a364333',
-      "clientSecret": 'e9d57363bbb4d5c20ddfbc00ec3f3769f1c8ac92',
-    },
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/bonybody",
-            "icon": "reco-github"
-          },
-          {
-            "text": "Twitter",
-            "link": "https://twitter.com/bony_body",
-            "icon": "reco-twitter"
-          }
+    "title": "DAYS PUT",
+    "description": "日々得られる知識を整頓する技術系備忘録",
+    "dest": "public",
+    "head": [
+        [
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "stylesheet",
+                "href": "https://fonts.googleapis.com/css?family=Anton"
+            }
+        ],
+        [
+            "link",
+            {
+                "rel": "stylesheet",
+                "href": "https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width,initial-scale=1,user-scalable=no"
+            }
         ]
-      }
     ],
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
+    "theme": "reco",
+    "themeConfig": {
+        "domain": 'https://daysput.netlify.app',
+        "vssueConfig": {
+            "platform": 'github',
+            "owner": 'bonybody',
+            "repo": 'daysput',
+            "clientId": '3c1c6993671e8a364333',
+            "clientSecret": 'e9d57363bbb4d5c20ddfbc00ec3f3769f1c8ac92',
+        },
+        "nav": [
+            {
+                "text": "Home",
+                "link": "/",
+                "icon": "reco-home"
+            },
+            {
+                "text": "TimeLine",
+                "link": "/timeline/",
+                "icon": "reco-date"
+            },
+            {
+                "text": "Contact",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "GitHub",
+                        "link": "https://github.com/bonybody",
+                        "icon": "reco-github"
+                    },
+                    {
+                        "text": "Twitter",
+                        "link": "https://twitter.com/bony_body",
+                        "icon": "reco-twitter"
+                    }
+                ]
+            }
+        ],
+        "type": "blog",
+        "blogConfig": {
+            "category": {
+                "location": 2,
+                "text": "Category"
+            },
+            "tag": {
+                "location": 3,
+                "text": "Tag"
+            }
+        },
+        "logo": "/logo.png",
+        "search": true,
+        "searchMaxSuggestions": 10,
+        "lastUpdated": "Last Updated",
+        "author": "I.Y",
+        "authorAvatar": "/avatar.png",
+        "record": "xxxx",
+        "startYear": "2020",
+        "mode": "light",
+        "modePicker": false
     },
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "I.Y",
-    "authorAvatar": "/avatar.png",
-    "record": "xxxx",
-    "startYear": "2020",
-    "mode": "light",
-    "modePicker": false
-  },
-  "markdown": {
-    "lineNumbers": true
-  },
-  plugins: {
-    '@vuepress/google-analytics': {
-      'ga': 'UA-173615878-1'
+    "markdown": {
+        "lineNumbers": true
     },
-    'seo': {
-      description: ($page, $site) => $page.frontmatter.description || ($page.excerpt && $page.excerpt.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")) || $site.description || "",
-      title: ($page, $site) => $page.title || $site.title,
-      image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'http://placehold.jp/50/3d4070/ffffff/1200x630.png?css=%7B%22word-wrap%22%3A%22%20break-word%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23667eea)%2C%20to(%23764ba2))%22%7D&text='+encodeURIComponent($page.title||$site.title),
+    plugins: {
+        '@vuepress/google-analytics': {
+            'ga': 'UA-173615878-1'
+        },
+        'seo': {
+            description: ($page, $site) => $page.frontmatter.description || ($page.excerpt && $page.excerpt.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")) || $site.description || "",
+            title: ($page, $site) => $page.title || $site.title,
+            image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image) || 'http://placehold.jp/50/3d4070/ffffff/1200x630.png?css=%7B%22word-wrap%22%3A%22%20break-word%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23667eea)%2C%20to(%23764ba2))%22%7D&text=' + encodeURIComponent($page.title || $site.title),
+        },
+        'social-share': {
+            networks: ['twitter', 'facebook', 'email'],
+            email: 'yumaaa312@gmail.com',
+            twitterUser: 'bony_body'
+        }
+
     }
-  }
 }
